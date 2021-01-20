f8ab7ace18b3e5f2397435660d97aebfbb72463e
Jorge Luis <cokimaniac@gmail.com>
2021-01-20 01:04:38 -0400

    Debtor component modified. Fetch all debtors to state

--- src/components/Debtors.jsx
+++ src/components/Debtors.jsx
@@ -1,35 +1,49 @@
-import React, { Component, Fragment } from "react";
+import React, { Fragment, useEffect, useState } from "react";
+//
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPhone, faPlus } from '@fortawesome/free-solid-svg-icons'
 
 // services
 import Debtor from "../services/Debtor";
+// components
+import DebtorModal from "./DebtorModal";
 
-class Debtors extends Component {
-    constructor(props) {
-        super(props);
-        this.state = undefined;
-    }
-    async componentDidMount() {
-        try {
-            let debtors = await Debtor.list();
-            this.setState(debtors);
-        } catch (err) {
-            console.error(err)
+function Debtors(props) {
+
+    const [debtors, setDebtors] = useState([]);
+
+    useEffect(() => {
+        const fetchDebtors = async () => {
+            const result = await Debtor.list();
+            setDebtors(result);
         }
-    }
-
-    componentDidUpdate() {
-        console.log(this.state)
-    }
-    render() {
-        return (
-            <Fragment>
-                <h4 className="display-5">Debtors</h4>
-                <ul className="list-group">
-                    
-                </ul>
-            </Fragment>
-        );
-    }
+        fetchDebtors();
+    }, []);
+        
+    return (
+        <Fragment>
+            <h4 className="display-5">
+                Debtors&nbsp;<DebtorModal />
+            </h4>
+            <ul className="list-group">
+                {debtors.map(item => (
+                    <li className="list-group-item" key={item._id}>
+                        <div className="card-w-75">
+                            <div className="card-body">
+                                <h5 className="card-title">
+                                    <a href={"tel:" + item.phoneNumber}>
+                                        <FontAwesomeIcon icon={faPhone} />
+                                    </a>&nbsp; {item.fullName}
+                                </h5>
+                                <p className="card-text">
+                                    Debts: {item.ammounts.length > 3 ? (<span className="badge badge-danger">{item.ammounts.length}</span>) : (<span className="badge badge-primary">{item.ammounts.length}</span>) }
+                                </p>
+                            </div>
+                        </div>
+                    </li>
+                ))}
+            </ul>
+        </Fragment>
+    );
 }
-
 export default Debtors;
