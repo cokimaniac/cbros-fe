799d3cd04650ac218fde7e1f7df359dd6a65fd52
Jorge Luis <cokimaniac@gmail.com>
2021-02-04 01:43:44 -0400

    Ammount modify: ammount modification paid state Ammount retrieve: retrieve specific ammount by id

--- src/services/Ammount.js
+++ src/services/Ammount.js
@@ -1,17 +1,29 @@
 import Auth from "./Auth";
 
-class Debtor {
+class Ammount {
     static async list(debtorID) {
-        let url = `http://localhost:8000/debtors/${debtorID}`;
+        let url = `http://localhost:8000/debtors/${debtorID}/ammounts`;
         let response = await fetch(url, Auth.getHeader());
         return response.json();
     }
 
     static async create(data, debtorID) {
-        let url = `http://localhost:8000/debtors/${debtorID}`;
+        let url = `http://localhost:8000/debtors/${debtorID}/ammounts`;
         let response = await fetch(url, Auth.postHeader(data));
+        return response.json();
+    }
+
+    static async modify(debtorID, ammountID, state) {
+        let url = `http://localhost:8000/debtors/${debtorID}/ammounts/${ammountID}?state=${state}`;
+        let response = await fetch(url, Auth.patchHeader());
+        return response.json();
+    }
+
+    static async retrieve(debtorID, ammountID) {
+        let url = `http://localhost:8000/debtors/${debtorID}/ammounts/${ammountID}`;
+        let response = await fetch(url, Auth.getHeader());
         return response.json();
     }
 }
 
-export default Debtor
+export default Ammount
