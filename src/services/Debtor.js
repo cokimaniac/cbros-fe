16e1767ef6376c05e70f4add2a209efbf262f933
Jorge Luis <cokimaniac@gmail.com>
2021-02-04 01:46:16 -0400

    Debtor retrieve: Retrieve debtor by id

--- src/services/Debtor.js
+++ src/services/Debtor.js
@@ -12,6 +12,12 @@ class Debtor {
         let response = await fetch(url, Auth.postHeader(data));
         return response.json();
     }
+
+    static async retrieve(debtorId) {
+        let url = `http://localhost:8000/debtors/${debtorId}`;
+        let response = await fetch(url, Auth.getHeader());
+        return response.json();
+    }
 }
 
 export default Debtor
